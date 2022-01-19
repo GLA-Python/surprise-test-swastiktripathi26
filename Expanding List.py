5e8e5caf44f57916ab46729452f61415286d3196
swastiktripathi26 <91699294+swastiktripathi26@users.noreply.github.com>
2022-01-19 20:03:44 +0530

    Solution of Expanding List

--- Expanding List.py
+++ Expanding List.py
@@ -1,17 +1,17 @@
-def expanding(a):
-    c=len(a)
-    lst=[]
-    for i in range(c-1):
-        d=a[i]-a[i+1]
+def expanding_list(a):
+    x = len(a)
+    lst = []
+    for i in range(x-1):
+        d = a[i]-a[i+1]
         lst.append(abs(d))
-    m=0
+    m = 0
     for i in range(len(lst)-1):
-        if lst[i+1]>lst[i]:
-            m+=1
-    if m==len(lst)-1:
+        if lst[i+1] > lst[i]:
+            m += 1
+    if m == len(lst) - 1:
         return True
     else:
         return False
-a=eval(input())
-out=expanding(a)
+a = eval(input())
+out = expanding_list(a)
 print(out)
